aecfbb3722b75534b2aacb122c01b8307eb77bd6
Ricard <rcruz25@politecnics.barcelona>
2026-07-03 19:20:40 +0200

    clase 03

---
+++ datos.js
@@ -0,0 +1,27 @@
+let nombreCoche = "volvo";
+console.log(nombreCoche);
+
+let a = 5
+let b = 2
+let c= '4'
+console.log(a + b + c); // 74
+
+document.write(a + b + c);
+
+let z = a+b
+alert(z);
+
+
+let nombre = 'pepe' , apellido = 'perez', edad2 = 30
+// + como operador de concatenacion
+console.log(nombre + ' ' + apellido + ' tiene '  + edad2 + ' años');
+
+// operador modulo
+
+let d= 10
+let e = 2
+let f = 3
+
+console.log(d % e); // 0
+console.log(d % f); // 1
+
